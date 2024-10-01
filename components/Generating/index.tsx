7fd241f1cf6e78cba997c6912bd066d818008f6d
JJ <jerry7722@hotmail.com>
2024-10-01 13:36:19 +0800

    YADA TALK

--- components/Generating/index.tsx
+++ components/Generating/index.tsx
@@ -11,7 +11,7 @@ const Generating = ({ className }: GeneratingProps) => (
         } text-base`}
     >
         <Image
-            className="w-5 h-5 mr-4"
+            className="w-5 h-5 mr-4 animate-spin"
             src="/images/loading.png"
             width={20}
             height={20}
